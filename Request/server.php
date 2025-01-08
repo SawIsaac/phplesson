327609171a2cf45f18197d658744bc86960ed1b4
sawisaac <aunghtethtet295810@gmail.com>
2025-01-08 11:13:11 +0700

    request

---
+++ Request/server.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Server Site</h1>
+    <a href="client.php">Go to Client site</a>
+</body>
+</html>
