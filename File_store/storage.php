81c31d6cd8de4142190d84a7b48981fb7d4c4e08
sawisaac <aunghtethtet295810@gmail.com>
2025-01-09 14:29:59 +0700

    phplesson

---
+++ File_store/storage.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    $_SESSION['name'] = "session mgmg";
+    $name = "ma ma";
+    echo $name;
+?>
+
+<a href="storage_clientsite.php">Client site</a>
---
+++ File_store/storage_clientsite.php
@@ -0,0 +1,5 @@
+<a href="storage.php">Go to storage site</a>
+<?php
+    session_start();
+    echo $_SESSION['name'];
+?>
